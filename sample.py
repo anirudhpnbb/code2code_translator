5cfe625d0cdcf9b66e73c37957614841d63b2e1a
Anirudh Palaparthi <anirudh.p@techsophy.com>
2024-09-03 23:42:11 +0530

    removed sample.py

--- sample.py
+++
@@ -1,3 +0,0 @@
-a = "python\ndef calculate_factorial(num):\n    if num <= 1:\n        return 1\n    else:\n        return num * calculate_factorial(num - 1)\n\nnumber = int(input(\"Enter a number to calculate its factorial:\\n\"))\nfactorial = calculate_factorial(number)\n\nprint(f\"The factorial of {number} is {factorial}\")\n"
-
-print(a)
